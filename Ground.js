2b981fdefab0ebd3044dd2e381ee0633c196dfbd
roseieli <roymou2006@gmail.com>
2020-11-26 13:41:01 +0530

    i have added stoke in ground.js

--- Ground.js
+++ Ground.js
@@ -12,6 +12,7 @@ class Ground {
       var pos =this.body.position;
       rectMode(CENTER);
       fill("brown");
+      stroke("brown");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
